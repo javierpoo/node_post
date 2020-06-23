6f31a88ab8c336d57bfb2510d39447eec7508b57
javierpoo <javier.poo@gmail.com>
2020-06-23 15:52:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const { parse } = require('querystring');
 const server = http.createServer((req, res) => {
     if (req.method === 'POST') {
 
-       console.log("req.body=" + req.body);
+        console.log("req=" + req);
+
+        console.log("req.body=" + req.body);
         
     } 
     else {
